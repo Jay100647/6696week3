938c336bf8c91b9717e907e0412f91ec791d30e4
Jay <jjza6327@gmail.com>
2024-12-17 10:44:56 +0700

    Ex1

---
+++ ex2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- เพิ่ม Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&family=Pattaya&display=swap" rel="stylesheet">
+    <style>
+        body{
+            font-family: "Pattaya", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+}
+    </style>
+    <!--  สิ้นสุดเพิ่ม font -->
+    <title>ตัวอย่าง1</title>
+</head>
+<body>
+    <h1> โปรแกรม ภาษา HTML </h1>
+    664485005 นายจักรฤษณ์ มากรักษา <br>
+    หมู่เรียน 66/96 <br>
+    
+</body>
+</html>
